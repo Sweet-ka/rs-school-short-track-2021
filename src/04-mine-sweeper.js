3b7d61c6e49e5c43480a661267386fae7dafa612
Sweet-ka <sweet-ka@mail.ru>
2021-04-11 23:08:21 +0300

    feat: add decision tasks except 14

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,47 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrixNew = new Array(matrix.length);
+  let count;
+  let i;
+  let j;
+  for (i = 0; i < matrix.length; i++) {
+    matrixNew[i] = new Array(matrix[0].length);
+    for (j = 0; j < matrix[0].length; j++) {
+      count = 0;
+      if (matrix[i][j] === true) {
+        matrixNew[i][j] = 1;
+      } else {
+        if (i - 1 >= 0 && j - 1 >= 0 && matrix[i - 1][j - 1] === true) {
+          count++;
+        }
+        if (i - 1 >= 0 && matrix[i - 1][j] === true) {
+          count++;
+        }
+        if (i - 1 >= 0 && j + 1 < matrix[0].length && matrix[i - 1][j + 1] === true) {
+          count++;
+        }
+        if (j - 1 >= 0 && matrix[i][j - 1] === true) {
+          count++;
+        }
+        if (j + 1 < matrix[0].length && matrix[i][j + 1] === true) {
+          count++;
+        }
+        if (i + 1 < matrix.length && j - 1 >= 0 && matrix[i + 1][j - 1] === true) {
+          count++;
+        }
+        if (i + 1 < matrix.length && matrix[i + 1][j] === true) {
+          count++;
+        }
+        if (i + 1 < matrix.length && j + 1 < matrix[0].length && matrix[i + 1][j + 1] === true) {
+          count++;
+        }
+        matrixNew[i][j] = count;
+      }
+    }
+  }
+  return matrixNew;
 }
 
 module.exports = minesweeper;
